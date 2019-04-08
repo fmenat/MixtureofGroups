d540bf08d08742ee8173196ae2354413e11fc1fb
FMena14 <francisco.mena.13@sansano.usm.cl>
2019-04-08 01:03:21 -0300

    new labelme

--- Executions/run_LabelMe.sh
+++ Executions/run_LabelMe.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#cd LabelMe
-#python ../../run_LabelMe.py -M 3 -p ../../LabelMe -v 1 > result.txt
-#cd ..
-cd LabelMe_v2
+cd LabelMe_noflatten
+python ../../run_LabelMe.py -M 3 -p ../../LabelMe -v 1 > result.txt
+cd ..
+cd LabelMe_v2_noflatten
 python ../../run_LabelMe.py -M 3 -p ../../LabelMe -v 2 > result.txt
 cd ..
-cd LabelMe_v3
+cd LabelMe_v3_noflatten
 python ../../run_LabelMe.py -M 3 -p ../../LabelMe -v 3 > result.txt
 cd ..
